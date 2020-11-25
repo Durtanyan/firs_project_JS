82bbbe14840e9a93fd7bc57d6960563f4697475f
Durtanyan <lukins.ya@yandex.ru>
2020-11-25 14:30:36 +0300

    add cicle for

--- js_file/console_application.js
+++ js_file/console_application.js
@@ -8,12 +8,10 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 3; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
