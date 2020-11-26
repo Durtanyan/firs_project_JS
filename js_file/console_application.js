a49feb28b70835437fcdb121780db794bf3e5a1b
Durtanyan <lukins.ya@yandex.ru>
2020-11-26 09:35:05 +0300

    add function

--- js_file/console_application.js
+++ js_file/console_application.js
@@ -1,24 +1,57 @@
-const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
+    while (numberOfFilms == '' || isNaN(numberOfFilms) || numberOfFilms == null) {
+        numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
+    }
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    private: false,
 };
 
-for (let i = 0; i < 3; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', '');
-    const b = prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 3; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', '');
+        const b = prompt('На сколько оцените его?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-    } else {
-        i--;
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
     }
 }
 
-(personalMovieDB.count < 10) ? alert('Вы посмотрели мало фильмов.'):(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) ? alert('Вы средний зритель.') : alert('Вы много смотрите кино.');
+function detectedPersonalLevel() {
+    (personalMovieDB.count < 10) ? alert('Вы посмотрели мало фильмов.'):
+        (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) ? alert('Вы средний зритель.') :
+        alert('Вы много смотрите кино.');
+}
+
+function writeYourGenres(){
+    for (let i = 0; i <= 2; i++ ) {
+        const answer = prompt(`Ваш любимый жанр номер ${i + 1}: `, '');
+        if (answer != '' && answer != null && +isNaN(answer)) {
+            personalMovieDB.genres[i] = answer;
+        } else {
+            i--;
+        }
+    }
+}
+
+let showMyDB = () => personalMovieDB.private;
+
+if (showMyDB() === false) {
+    start();
+    rememberMyFilms();
+    writeYourGenres();
+    detectedPersonalLevel();
+}
 
 console.log(personalMovieDB);
